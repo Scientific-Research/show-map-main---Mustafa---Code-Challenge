990d39e771fa56eab8743082dee812314b4b6b9c
Maximilian <scientificresearch309@gmail.com>
2023-02-04 00:58:18 +0100

    Berlin Long and Lat ist wieder da!

--- src/hook/useMap.ts
+++ src/hook/useMap.ts
@@ -5,8 +5,8 @@ import { generateNewMarker } from "../utils/generateNewMarker";
 
 export const useMap = (container: React.RefObject<HTMLDivElement>) => {
   const mapInitRef = useRef<Map | null>(null);
-//   const mapInitRef2 = useRef<Map | null>(null);
-//   const mapInitRef3 = useRef<Map | null>(null);
+  //   const mapInitRef2 = useRef<Map | null>(null);
+  //   const mapInitRef3 = useRef<Map | null>(null);
 
   //Berlin, Germany
   useEffect(() => {
@@ -19,27 +19,27 @@ export const useMap = (container: React.RefObject<HTMLDivElement>) => {
     }
   }, []);
 
-//   // Paris, France
-//   useEffect(() => {
-//     if (container.current) {
-//       mapInitRef2.current = initMap(
-//         container.current,
-//         // [-100.31019063199852, 25.66901932031443]
-//         [2.349014, 48.864716]
-//       );
-//     }
-//   }, []);
+  //   // Paris, France
+  //   useEffect(() => {
+  //     if (container.current) {
+  //       mapInitRef2.current = initMap(
+  //         container.current,
+  //         // [-100.31019063199852, 25.66901932031443]
+  //         [2.349014, 48.864716]
+  //       );
+  //     }
+  //   }, []);
 
-//   // Brussels
-//   useEffect(() => {
-//     if (container.current) {
-//       mapInitRef3.current = initMap(
-//         container.current,
-//         // [-100.31019063199852, 25.66901932031443]
-//         [4.34878, 50.85045]
-//       );
-//     }
-//   }, []);
+  //   // Brussels
+  //   useEffect(() => {
+  //     if (container.current) {
+  //       mapInitRef3.current = initMap(
+  //         container.current,
+  //         // [-100.31019063199852, 25.66901932031443]
+  //         [4.34878, 50.85045]
+  //       );
+  //     }
+  //   }, []);
 
   useEffect(() => {
     mapInitRef.current &&
@@ -51,74 +51,74 @@ export const useMap = (container: React.RefObject<HTMLDivElement>) => {
       mapInitRef.current?.off("dblclick", generateNewMarker);
     };
   }, []);
-//   /////////////////////////
-//   useEffect(() => {
-//     mapInitRef2.current &&
-//       mapInitRef2.current.on("dblclick", ({ lngLat2 }) =>
-//         generateNewMarker({ map: mapInitRef2.current!, ...lngLat2 })
-//       );
+  //   /////////////////////////
+  //   useEffect(() => {
+  //     mapInitRef2.current &&
+  //       mapInitRef2.current.on("dblclick", ({ lngLat2 }) =>
+  //         generateNewMarker({ map: mapInitRef2.current!, ...lngLat2 })
+  //       );
 
-//     return () => {
-//       mapInitRef2.current?.off("dblclick", generateNewMarker);
-//     };
-//   }, []);
-//   ////////////////////////////////
+  //     return () => {
+  //       mapInitRef2.current?.off("dblclick", generateNewMarker);
+  //     };
+  //   }, []);
+  //   ////////////////////////////////
 
-//   /////////////////////////
-//   useEffect(() => {
-//     mapInitRef3.current &&
-//       mapInitRef3.current.on("dblclick", ({ lngLat3 }) =>
-//         generateNewMarker({ map: mapInitRef3.current!, ...lngLat3 })
-//       );
+  //   /////////////////////////
+  //   useEffect(() => {
+  //     mapInitRef3.current &&
+  //       mapInitRef3.current.on("dblclick", ({ lngLat3 }) =>
+  //         generateNewMarker({ map: mapInitRef3.current!, ...lngLat3 })
+  //       );
 
-//     return () => {
-//       mapInitRef3.current?.off("dblclick", generateNewMarker);
-//     };
-//   }, []);
-//   ////////////////////////////////
+  //     return () => {
+  //       mapInitRef3.current?.off("dblclick", generateNewMarker);
+  //     };
+  //   }, []);
+  //   ////////////////////////////////
 
-//   useEffect(() => {
-//     mapInitRef.current &&
-//       mapInitRef.current.on("load", () =>
-//         generateNewMarker({
-//           map: mapInitRef.current!,
-//           ...mapInitRef.current!.getCenter(),
-//         })
-//       );
+  useEffect(() => {
+    mapInitRef.current &&
+      mapInitRef.current.on("load", () =>
+        generateNewMarker({
+          map: mapInitRef.current!,
+          ...mapInitRef.current!.getCenter(),
+        })
+      );
 
-//     return () => {
-//       mapInitRef.current?.off("load", generateNewMarker);
-//     };
-//   }, []);
-//   ////////////////////////////////
-//   useEffect(() => {
-//     mapInitRef2.current &&
-//       mapInitRef2.current.on("load", () =>
-//         generateNewMarker({
-//           map: mapInitRef2.current!,
-//           ...mapInitRef2.current!.getCenter(),
-//         })
-//       );
+    return () => {
+      mapInitRef.current?.off("load", generateNewMarker);
+    };
+  }, []);
+  //   ////////////////////////////////
+  //   useEffect(() => {
+  //     mapInitRef2.current &&
+  //       mapInitRef2.current.on("load", () =>
+  //         generateNewMarker({
+  //           map: mapInitRef2.current!,
+  //           ...mapInitRef2.current!.getCenter(),
+  //         })
+  //       );
 
-//     return () => {
-//       mapInitRef2.current?.off("load", generateNewMarker);
-//     };
-//   }, []);
-//   ///////////////////////////////
+  //     return () => {
+  //       mapInitRef2.current?.off("load", generateNewMarker);
+  //     };
+  //   }, []);
+  //   ///////////////////////////////
 
-//   ////////////////////////////////
-//   useEffect(() => {
-//     mapInitRef3.current &&
-//       mapInitRef3.current.on("load", () =>
-//         generateNewMarker({
-//           map: mapInitRef3.current!,
-//           ...mapInitRef3.current!.getCenter(),
-//         })
-//       );
+  //   ////////////////////////////////
+  //   useEffect(() => {
+  //     mapInitRef3.current &&
+  //       mapInitRef3.current.on("load", () =>
+  //         generateNewMarker({
+  //           map: mapInitRef3.current!,
+  //           ...mapInitRef3.current!.getCenter(),
+  //         })
+  //       );
 
-//     return () => {
-//       mapInitRef3.current?.off("load", generateNewMarker);
-//     };
-//   }, []);
-//   ///////////////////////////////
+  //     return () => {
+  //       mapInitRef3.current?.off("load", generateNewMarker);
+  //     };
+  //   }, []);
+  //   ///////////////////////////////
 };
