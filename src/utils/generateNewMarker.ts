289b60654493008ee3a2c36c1bb5b6e87f1f2283
Maximilian <scientificresearch309@gmail.com>
2023-02-03 23:57:22 +0100

    scale: 2.5

--- src/utils/generateNewMarker.ts
+++ src/utils/generateNewMarker.ts
@@ -5,7 +5,7 @@ export const generateNewMarker = ({ lat, lng, map }: { lng: number, lat: number,
     const popUp = new Popup({ closeButton: false, anchor: 'right', })
         .setHTML(`<div class="popup">You click here: <br/>[${lng},  ${lat}]</div>`)
 
-    new Marker({ color: '#63df29', scale: 1.5 })
+    new Marker({ color: '#63df29', scale: 2.5 })
         .setLngLat([lng, lat])
         .setPopup(popUp)
         .addTo(map)
