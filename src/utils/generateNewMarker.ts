1a1f642d2c3199761ad356fd72a431129a2cff58
Maximilian <scientificresearch309@gmail.com>
2023-02-03 23:56:40 +0100

    anchor: 'right'

--- src/utils/generateNewMarker.ts
+++ src/utils/generateNewMarker.ts
@@ -2,7 +2,7 @@ import { Popup, Marker, Map } from 'mapbox-gl';
 
 export const generateNewMarker = ({ lat, lng, map }: { lng: number, lat: number, map: Map }) => {
 
-    const popUp = new Popup({ closeButton: false, anchor: 'left', })
+    const popUp = new Popup({ closeButton: false, anchor: 'right', })
         .setHTML(`<div class="popup">You click here: <br/>[${lng},  ${lat}]</div>`)
 
     new Marker({ color: '#63df29', scale: 1.5 })
