7d19c2680e1953d0bbe9372eb7b754db00c9fcc1
Maximilian <scientificresearch309@gmail.com>
2023-02-04 02:23:49 +0100

    Berlin works with Berlin.json Data

--- src/hook/useMap.ts
+++ src/hook/useMap.ts
@@ -2,19 +2,38 @@ import { useEffect, useRef } from "react";
 import { Map } from "mapbox-gl";
 import { initMap } from "../utils/initMap";
 import { generateNewMarker } from "../utils/generateNewMarker";
+import BerlinLatLng from "../data/BerlinLatLng.json";
 
 export const useMap = (container: React.RefObject<HTMLDivElement>) => {
   const mapInitRef = useRef<Map | null>(null);
   //   const mapInitRef2 = useRef<Map | null>(null);
   //   const mapInitRef3 = useRef<Map | null>(null);
 
+  const BLL0 = () => {
+    const a = Number(BerlinLatLng.map((BL0) => BL0.capitalInfo.latlng[0]));
+    console.log(a);
+    return a;
+  };
+  const BLL1 = () => {
+    const b = Number(BerlinLatLng.map((BL0) => BL0.capitalInfo.latlng[1]));
+    console.log(b);
+    return b;
+  };
+  //   BLL.capitalInfo.latlng[1];
+
+  //   BLL();
   //Berlin, Germany
   useEffect(() => {
+    const c = BLL0();
+    const d = BLL1();
+    console.log(c);
+    console.log(d);
     if (container.current) {
       mapInitRef.current = initMap(
         container.current,
         // [-100.31019063199852, 25.66901932031443]
-        [13.404954, 52.520008]
+        // [13.404954, 52.520008]
+        [d, c]
       );
     }
   }, []);
